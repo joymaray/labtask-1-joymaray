889a29ff339faf09afa0db9c0bcafea45598847e
joymaray <joymaray8@gmail.com>
2025-02-03 12:12:01 -0500

    hi

---
+++ GetMyStuff.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "User ID: $(id -u)"
+echo "Primary Group ID: $(id -g)"
+echo "Groups: $(groups)"
+echo "Shell Path: $SHELL"
+echo "Home Directory: $HOME"
+echo "PATH Variable: $PATH"
